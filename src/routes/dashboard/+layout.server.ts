0932e2772a98569524c5f1edec863f90714701a4
NicolaeGr <114419701+NicolaeGr@users.noreply.github.com>
2024-01-03 16:56:16 +0200

    load profiles and links inside /dashboard layout

--- src/routes/dashboard/+layout.server.ts
+++ src/routes/dashboard/+layout.server.ts
@@ -1,10 +1,41 @@
 import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
+import {
+	getLinksMetafieldByCustomerIdQuery,
+	getProfilesMetafieldByCustomerIdQuery
+} from '$utils/queries.admin';
+import { admin } from '$utils/shopify';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
 	const user = await locals.getSession();
 
 	if (!user) redirect(302, '/login');
 
-	return { user: user };
+	const resultData: any = { user: user };
+
+	const [profilesResponse, linksResponse] = await Promise.all([
+		admin.request(getProfilesMetafieldByCustomerIdQuery, {
+			variables: {
+				customer_id: user.id
+			}
+		}),
+		admin.request(getLinksMetafieldByCustomerIdQuery, {
+			variables: {
+				customer_id: user.id
+			}
+		})
+	]);
+
+	if (!profilesResponse.data || !linksResponse.data) {
+		console.log(JSON.stringify(profilesResponse, null, 2));
+		console.log(JSON.stringify(linksResponse, null, 2));
+	}
+
+	const profiles = JSON.parse(profilesResponse.data.customer.profiles?.value || '[]');
+	const links = JSON.parse(linksResponse.data.customer.links?.value || '[]');
+
+	resultData.profiles = profiles;
+	resultData.links = links;
+
+	return resultData;
 };
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -4,7 +4,7 @@
 	export let data: PageData;
 </script>
 
-<a href="/" class="text-blue-500">Go Home</a>
+<a href="/" class="text-blue-600">Go Home</a>
 
 <br /><br />
 <hr />
@@ -12,3 +12,9 @@
 <pre>
 {JSON.stringify(data.user, null, '\t')}
 </pre>
+
+<br /><br />
+
+<span>Profiles: {JSON.stringify(data.profiles)}</span>
+<br />
+<span>Links: {JSON.stringify(data.links)}</span>
